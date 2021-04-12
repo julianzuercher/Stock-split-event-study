785dc395a47f7704033514d5cb4fe085406ea794
julianzuercher <julian.zuercher@student.unisg.ch>
2021-04-12 16:57:57 +0200

    "event study"

--- stock-split-code.R
+++ stock-split-code.R
@@ -5,15 +5,11 @@
 
 # Working Directory --------------------------------------------------------------------------------------
 
-getwd()
+# getwd()
 # setwd("C:/Users/juzu/Desktop/MBF/Research Seminar Corporate Finance/data")
 
 
 # Packages & Libraries -----------------------------------------------------------------------------------
-
-#install.packages("ff")
-#install.packages("ffbase")
-#install.packages("RPostgres")
 
 library("ff")
 library("ffbase")
@@ -86,13 +82,13 @@ split_df_3 <- split_df_2[split_df_2$share_code==11,]
 # Expanding event window around declaration date to clear for all confounding events in 10 day period around split announcement:
 
 split_df_3$declaration_date <- as.Date(format(split_df_3$declaration_date, format = '%Y%m%d'), format = '%Y%m%d')
-
+#After announcement date
 split_df_3$declaration_date_1_post <- split_df_3$declaration_date+1
 split_df_3$declaration_date_2_post <- split_df_3$declaration_date+2
 split_df_3$declaration_date_3_post <- split_df_3$declaration_date+3
 split_df_3$declaration_date_4_post <- split_df_3$declaration_date+4
 split_df_3$declaration_date_5_post <- split_df_3$declaration_date+5
-
+#Before announcement date
 split_df_3$declaration_date_1_pre <- split_df_3$declaration_date-1
 split_df_3$declaration_date_2_pre <- split_df_3$declaration_date-2
 split_df_3$declaration_date_3_pre <- split_df_3$declaration_date-3
@@ -100,13 +96,13 @@ split_df_3$declaration_date_4_pre <- split_df_3$declaration_date-4
 split_df_3$declaration_date_5_pre <- split_df_3$declaration_date-5
 
 split_df_3$declaration_date<-as.numeric(format(split_df_3$declaration_date, format = '%Y%m%d'))
-
+#After announcement date
 split_df_3$declaration_date_1_post<-as.numeric(format(split_df_3$declaration_date_1_post, format = '%Y%m%d'))
 split_df_3$declaration_date_2_post<-as.numeric(format(split_df_3$declaration_date_2_post, format = '%Y%m%d'))
 split_df_3$declaration_date_3_post<-as.numeric(format(split_df_3$declaration_date_3_post, format = '%Y%m%d'))
 split_df_3$declaration_date_4_post<-as.numeric(format(split_df_3$declaration_date_4_post, format = '%Y%m%d'))
 split_df_3$declaration_date_5_post<-as.numeric(format(split_df_3$declaration_date_5_post, format = '%Y%m%d'))
-
+#Before announcement date
 split_df_3$declaration_date_1_pre <-as.numeric(format(split_df_3$declaration_date_1_pre, format = '%Y%m%d'))
 split_df_3$declaration_date_2_pre <-as.numeric(format(split_df_3$declaration_date_2_pre, format = '%Y%m%d'))
 split_df_3$declaration_date_3_pre <-as.numeric(format(split_df_3$declaration_date_3_pre, format = '%Y%m%d'))
@@ -114,7 +110,7 @@ split_df_3$declaration_date_4_pre <-as.numeric(format(split_df_3$declaration_dat
 split_df_3$declaration_date_5_pre <-as.numeric(format(split_df_3$declaration_date_5_pre, format = '%Y%m%d'))
 
 
-# Confounding Events -------------------------------------------------------
+# Confounding Events -------------------------------------------------------------------------------------
 
 # -> elimminating effects that could effect stock returns on announcement date
 #PERMNO's which conducted stocksplits (2000 - 2020) and where the declaration date is available (see unique_identifier)
@@ -209,37 +205,41 @@ confounding_10days_small <- confounding_10days[,c(1:19)]
 #write.csv2(confounding_10days,"confounding_10days.csv",row.names=FALSE)
 
 
-# Further filter Criteria ---------------------------------------------------------
+# Further filter criteria --------------------------------------------------------------------------------
 
-confounding_10days_small$market_cap <- as.numeric(as.character(confounding_10days_small$shares_oustanding_in_ks))*1000*as.numeric(as.character(confounding_10days_small$share_price))
 
-non_conf_large <- confounding_10days_small[confounding_10days_small$market_cap> 0,]
+confounding_10days_small$market_cap <- 
+  as.numeric(as.character(confounding_10days_small$shares_oustanding_in_ks))*1000*as.numeric(as.character(confounding_10days_small$share_price))
 
+non_conf_large <- confounding_10days_small[confounding_10days_small$market_cap> 0,]
 non_conf_df <- non_conf_large[as.numeric(as.character(non_conf_large$factor_to_adjust_price_FACPR))>0.25,]
-
 non_conf_df$year <- as.numeric(substr(non_conf_df$declaration_date, 1, 4))
-
 non_conf_df_10_19 <- non_conf_df[(non_conf_df$year>=2010) & (non_conf_df$year<=2019),]
 
 final_df<-unique(non_conf_df_10_19)
 final_df_2<-na.omit(final_df)
 
+#Conversion from factors to numeric variables
 final_df_2$permco <- as.numeric(as.character(final_df_2$permco))
 final_df_2$trading_volume <- as.numeric(as.character(final_df_2$trading_volume))
+final_df_2$permno <- as.numeric(as.character(final_df_2$permno))
 
 final_df_3 <- final_df_2[order(final_df_2$trading_volume, decreasing = TRUE),]
 final_df_4 <- final_df_3[!duplicated(final_df_3$permco) | !duplicated(final_df_3$declaration_date),]
 #delete duplicated values with lowest trading volume
 
-
 write.csv2(final_df_4,"final_df_10_19_ff.csv",row.names=FALSE)
 
-# Return data for split titles --------------------------------------------
+
+# Return / Market data for split titles ---------------------------------------------------------------------------
+
 
 #Market return during 2000-2020 (NYSE,AMEX,NASDAQ - value weighted)
 #@https://wrds-web.wharton.upenn.edu/wrds//ds/crsp/indexes_a/mktindex/cap_d.cfm
 
 market_return_df <- read.csv("market_returns.csv")
+colnames(market_return_df)[1]<-"date"
+market_return_df$date <- as.Date(format(market_return_df$date , format = '%Y%m%d'), format = '%Y%m%d')
 
 # Daily stock returns: direct SQL querry from WRDS 
 # -> requires an account and storage of account name / PW on local file, see below:
@@ -280,23 +280,21 @@ final_df_4$declaration_date <- as.Date(format(final_df_4$declaration_date, forma
 #Check if all declaration dates are trading dates on the exchanges
 all(final_df_4$declaration_date %in% dates_trades)
 
+#Event window
 final_df_4$start_date <- final_df_4$declaration_date-10
 final_df_4$announce_date_and_1 <-final_df_4$declaration_date+1
 final_df_4$end_date <-final_df_4$declaration_date+10
 
-final_df_4$permno <- as.numeric(as.character(final_df_4$permno))
-
+#Start of estimation period
+final_df_4$est_start_date <-final_df_4$declaration_date-220
 
 get_stock_return_until_announcement <- function(data.frame) {
   data_1<- tryCatch(
     {
       
       a<- as.character(data.frame$permno[1])
-      
       b<- as.character(data.frame$start_date[1])
-      
       c<- as.character(data.frame$announce_date_and_1[1])
-      
       d<-sprintf("select ret
                  from crsp.dsf
                  where permno = '%s'
@@ -304,11 +302,8 @@ get_stock_return_until_announcement <- function(data.frame) {
                  and '%s'", a,b,c)
       
       res <- dbSendQuery(wrds, d)
-      
       data <- dbFetch(res, n=-1)
-      
       dbClearResult(res)
-      
       return(data)
       
     },
@@ -328,11 +323,8 @@ get_stock_return_after_announcement <- function(data.frame) {
     {
       
       a<- as.character(data.frame$permno[1])
-      
       b<- as.character(data.frame$declaration_date[1])
-      
       c<- as.character(data.frame$end_date[1])
-      
       d<-sprintf("select ret
                  from crsp.dsf
                  where permno = '%s'
@@ -340,11 +332,8 @@ get_stock_return_after_announcement <- function(data.frame) {
                  and '%s'", a,b,c)
       
       res <- dbSendQuery(wrds, d)
-      
       data <- dbFetch(res, n=-1)
-      
       dbClearResult(res)
-      
       return(data)
       
     },
@@ -368,18 +357,14 @@ for (i in 1:nrow(final_df_4)){
 }
 
 return_df_before_announcement <- as.data.frame(return_df_before_announcement)
-
 return_dt_before_announcement <- data.table(return_df_before_announcement)
-
 return_df_until_announcement<- as.data.frame(return_dt_before_announcement[, lapply(.SD, function(x) tail(x[!is.na(x)],6))])
-
 
 for (i in 1:ncol(return_df_before_announcement)){
   return_df_until_decl[,i]<- return_df_before_announcement[first_value[i]:last_non_NA[i],i]
 }
 
 return_df_until_decl <- return_df_before_announcement[last_non_NA-6:last_non_NA,]
-
 return_df_after_announcement <- matrix(data=NA, nrow= 20, ncol= nrow(final_df_4))
 
 for (i in 1:nrow(final_df_4)){
@@ -390,28 +375,68 @@ for (i in 1:nrow(final_df_4)){
 }
 
 return_df_after_announcement <- as.data.frame(return_df_after_announcement)
-
 return_df_after_announcement <- return_df_after_announcement[1:5,]
 
 eventwindow_df <- rbind(return_df_until_announcement,return_df_after_announcement)
+colnames(eventwindow_df)<-final_df_4$ticker
+rownames(eventwindow_df)<- c("t=-5","t=-4","t=-3","t=-2","t=-1","t=0","t=+1","t=+2","t=+3","t=+4","t=+5")
 
-a<- as.character(final_df_4$permno[2])
+get_stock_beta_from_estimation <- function(data.frame) {
+  data_1<- tryCatch(
+    {
+      
+      a<- as.character(data.frame$permno[1])
+      b<- as.character(data.frame$est_start_date[1])
+      c<- as.character(data.frame$declaration_date[1])
+      d<-sprintf("select date,ret
+                 from crsp.dsf
+                 where permno = '%s'
+                 and date between '%s'
+                 and '%s'", a,b,c)
+      
+      res <- dbSendQuery(wrds, d)
+      data <- dbFetch(res, n=-1)
+      dbClearResult(res)
+      data_with_gap <- head(data,-10)
+      data_with_gap_120 <- tail(data_with_gap,120)
+      rets<-merge(data_with_gap_120,market_return_df,by="date",all.x="true")
+      regression<-lm(rets$vwretd ~ rets$ret)
+      return(regression$coefficients)
+      
+    },
+    error = function(e){
+      message('Error in the process')
+      print(e)
+      dbClearResult(res)
+    }
+    
+    
+      )
+}
 
-b<- as.character(final_df_4$start_date[2])
+alphas_betas_df <- matrix(data=NA, nrow= 2, ncol= nrow(final_df_4))
 
-c<- as.character(final_df_4$end_date[2])
+for (i in 1:nrow(final_df_4)){
+  sub_df <- final_df_4[i,]
+  alphas_betas_df[,i] <- get_stock_beta_from_estimation(sub_df)
+}
 
-d<-sprintf("select date,ret
-           from crsp.dsf
-           where permno = '%s'
-           and date between '%s'
-           and '%s'", a,b,c)
+alphas_betas_df <- as.data.frame(alphas_betas_df)
 
-res <- dbSendQuery(wrds, d)
+rownames(alphas_betas_df)<-c("alpha","beta")
+colnames(alphas_betas_df)<-final_df_4$ticker
 
+market_model_df <- matrix(data=NA, nrow= 11, ncol= nrow(final_df_4))
 
-data <- dbFetch(res, n=-1)
-dbClearResult(res)
-data
+for (i in 1:nrow(final_df_4)){
+  sub_df <- final_df_4[i,]
+  indx <- match(sub_df$declaration_date,market_return_df$date)
+  m_returns <- market_return_df$vwretd[(indx-5):(indx+5)]
+  est_returns<-m_returns*alphas_betas_df[2,i]+alphas_betas_df[1,i]
+  market_model_df[,i]<-est_returns
+}
 
+car_df <- eventwindow_df - market_model_df
+
+write.csv2(car_df,"car_df.csv",row.names=TRUE)
 
