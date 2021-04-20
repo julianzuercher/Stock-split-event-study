b164751d3ccb990893881939dd3c2f3c25dff306
julianzuercher <julian.zuercher@student.unisg.ch>
2021-04-20 13:07:33 +0200

    "comments final"

--- stock-split-code.R
+++ stock-split-code.R
@@ -5,8 +5,9 @@
 
 # Working Directory --------------------------------------------------------------------------------------
 
+# If no R-project is set up:
 # getwd()
-# setwd("C:/Users/juzu/Desktop/MBF/Research Seminar Corporate Finance/data")
+# setwd("C:/Users/XXXX/XXXX)
 
 
 # Packages & Libraries -----------------------------------------------------------------------------------
@@ -20,13 +21,14 @@ library("tidyverse")
 library("RPostgres")
 library("data.table")
 
+
 # Data gathering for stock split equity titles  ----------------------------------------------------------
 
 #CRSP Database: Daily stock files, 01.01.2010 - 31.12.2020 
 #entire database
 #@https://wrds-web.wharton.upenn.edu/wrds//ds/crsp/stock_a/
 
-# raw_data.ff <- read.table.ffdf(file="2010_2020.csv", sep=",")
+#Due to size of data sets load in files for 2010 - 2020 and for 2000 - 2010  seperately with ff packages
 
 raw_data.ff <- read.table.ffdf(file="2010_2020_cusip.csv", sep=",")
 
@@ -34,7 +36,6 @@ colnames_wrds <- c("permno","date", "share_code","exchange_code","ticker","compa
                    "primary_exchange","permco","header_exchange_code","declaration_date","payment_date",
                    "distribution_code","factor_to_adjust_price_FACPR","factor_to_adjust_shares_FACSHR",
                    "share_price","trading_volume","shares_oustanding_in_ks", "number_of_trades")
-
 df_splits_2010_2020=raw_data.ff[raw_data.ff$distribution_code==5523,]
 
 #table.ff(df_splits_2010_2020$distribution_code)
@@ -44,38 +45,38 @@ df_splits_2010_2020=raw_data.ff[raw_data.ff$distribution_code==5523,]
 #p. 130: split
 
 df_2010_2020<- as.data.frame(df_splits_2010_2020)
-
-#due to size of data sets: load in files for 2010 - 2020 and for 2000 - 2010  seperateky
-
 raw_data.ff2 <- read.table.ffdf(file="2000_2010.csv", sep=",")
 colnames(raw_data.ff2) <- colnames_wrds
 df_splits_2000_2010 <- raw_data.ff2[raw_data.ff2$distribution_code==5523,]
 df_2000_2010<- as.data.frame(df_splits_2000_2010)
 
+#Merged dataset
 split_df <- rbind(df_2000_2010,df_2010_2020)
 #write.csv(split_df,"stocksplit_df_raw.csv",row.names=FALSE)
-
-split_df<-read.csv2("stocksplit_df_raw.csv")
 
 rm(raw_data.ff)
 rm(raw_data.ff2)
 
+# Shortcut to prepared data ------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+split_df<-read.csv2("stocksplit_df_raw.csv")
+# --------------------------------------------------------------------------------------------------------
+
 split_df_2 <- split_df[(!is.na(split_df$declaration_date)) & (split_df$declaration_date != ""),]
 #only splits where the delcaration date / "announcement date" was recorded
-
 split_df_2$year <- as.numeric(substr(split_df_2$declaration_date, 1, 4))
 split_df_3 <- split_df_2[(split_df_2$year>=2010) & (split_df_2$year<=2019),]
 #focus on 2010 - 2019 to avoid distortions due to the GFC and COVID 19
 split_df_4 <- split_df_3[split_df_3$share_code==11,]
 #only common shares - as in empirical literature
 
-
-
-length(unique_permnos<-unique(split_df_2$permno))
+unique_permnos<-unique(split_df_2$permno)
 #write.table(unique_permnos,"permnos.txt",row.names = FALSE, col.names = FALSE, quote = FALSE)
-#using the permnos to construct the lookup table / linking table to later match the stock splits to the confounding events (CAP-IQ)
 
-# Expanding event window around declaration date to clear for all confounding events in 10 day period around split announcement:
+#Later the extracted permnos will be used to download the lookup table / linking table
+#THe linking table will then later match the stock splits to the confounding events
+
+#Expanding event window around declaration date to clear for all confounding events in 10 day period around split announcement:
 
 split_df_4$declaration_date <- as.Date(format(split_df_4$declaration_date, format = '%Y%m%d'), format = '%Y%m%d')
 #After announcement date
@@ -108,9 +109,11 @@ split_df_4$declaration_date_5_pre <-as.numeric(format(split_df_4$declaration_dat
 
 # Confounding Events -------------------------------------------------------------------------------------
 
-#elimminating effects that could effect stock returns on announcement date:
-#PERMNO's which conducted stocksplits (2010 - 2019) and where the declaration date is available (see unique_permnos)
-# -> loaded in to the linking table to receive the respective GVKEY
+#Elimminating effects that could affect stock returns on announcement date / in event window:
+
+#PERMNO's which conducted stocksplits (2010 - 2019) and where the declaration date is available (see unique_permnos) 
+#were loaded into the linking table to receive the respective GVKEY
+
 #Linked with CRSP/Compustat Merged Database (under CRSP) - Linking Table (to  GVKEY)
 #@https://wrds-web.wharton.upenn.edu/wrds//ds/crsp/ccm_a/linktable/index.cfm#
 
@@ -120,25 +123,24 @@ colnames(linking_table)[8]<-"ticker"
 
 linking_table_small<-linking_table[,c("GVKEY","ticker","permno")]
 linking_table_small_u<-unique(linking_table_small<-linking_table[,c("GVKEY","ticker","permno")])
+#Matches  split data frame from PERMNO to the GVKEY identifiers
 
 split_df_5<-merge(x = split_df_4, y = linking_table_small_u, by = c("permno","ticker"), all.x = TRUE)
 split_df_6 <- na.omit(split_df_5)
-#matched the split data frame to the GVKEY identifiers
+
+#unique_GVKEY <-unique(linking_table_small_u$GVKEY)
+#write.table(unique_GVKEY,"gvkey_confounding.txt",row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 #WRDS: Capital IQ - Key Developments, 01.01.2000 - 21.11.2020 
-# - used linked- GVKEYS from linking tool to get all the company announcements related to the LPERMNO's which have seen a stock split
+# - used linked- GVKEYS from linking tool to get all the company announcements / news related to the LPERMNO's which have seen a stock split
 # - Capital IQ provides all the confounding events for the GVKEY in a data set
 
-# *Note: The "confounding_df" includes all confounding events from 01.01.2000 - 21.11.2020 for all companies to which
-# a link could be found with the methodology described above. So it contains more 
-# GVKEYS from splitting companies than the matched "splitd_df_6" which only focuses on 2010-2019. The reason being,
-# that analysis were conducted with the extended data set later than omited. However, the confounding data includes the subset of 2010-2019:
-
-#linking_table_extensive <- read.csv("linking_table.csv")
-#unique_permnos <-unique(linking_table_extensive$LPERMNO)
-#write.table(unique_permnos,"permno_confounding.txt",row.names = FALSE, col.names = FALSE, quote = FALSE)
-#unique_GVKEY <-unique(linking_table_extensive$GVKEY)
-#write.table(unique_GVKEY,"gvkey_confounding.txt",row.names = FALSE, col.names = FALSE, quote = FALSE)
+#*Note: The "confounding_df" includes all confounding events from 01.01.2000 - 21.11.2020 for all companies to which
+#a link could be found with the methodology described above. So it contains more GVKEYS from splitting companies than the matched "splitd_df_6" 
+#This data frame focuses on 2010-2019. The reason being,initially further analysis were conducted with the extended data set (2000-2020).
+#However, due to the market shocks thos analysis were than neglected 
+#To avoid downloading (timely) a redudant and smaller data set, the larger data frame (confounding_GVKEY.csv) was used.
+#It obivously includes all the confounding events in 2010-2020 from all companies conducting stock splits.
 
 #@https://wrds-web.wharton.upenn.edu/wrds//ds/comp/ciq/keydev/index.cfm
 
@@ -146,14 +148,15 @@ confounding_df <- read.csv("confounding_GVKEY.csv")
 colnames(confounding_df)[4]<-"declaration_date"
 colnames(confounding_df)[6]<-"GVKEY"
 
-
-# key_development_lookup <- read.csv2("key_development_lookup.csv")
-# colnames(key_development_lookup)[1] <- "keydeveventtypeid"
-# MANUAL CHECK: WHICH EVENTS SHOULD BE EXCLUDED
+#key_development_lookup <- read.csv2("key_development_lookup.csv")
+#colnames(key_development_lookup)[1] <- "keydeveventtypeid"
+#MANUAL CHECK: WHICH EVENTS SHOULD BE EXCLUDED
 
 confounding_df=confounding_df[!confounding_df$keydeveventtypeid %in% c(53,55,78,140,144),]
-#removing only superficial confounding events from the black list that will bear
+#Removing only superficial confounding events from the black list that will bear
 #no information to the public (expected announcement date estimations, certain company meeting dates etc.)
+
+#Merging all the dates (10 days around any announcement date of a stock split) with confounding events via  date and identifier (GVKEY)
 
 merged_df_1<-left_join(x = split_df_6, y = confounding_df[,c(3,4,6)], by = c("declaration_date","GVKEY"))
 
@@ -204,8 +207,8 @@ merged_df_11<-unique(merged_df_11)
 
 rm(merged_df_1,merged_df_2,merged_df_3,merged_df_4,merged_df_5,merged_df_6,merged_df_7,merged_df_8,merged_df_9,merged_df_10)
 
-#after looking up all different events provided by Capital IQ, all confounding events in a 10-day window around the stock split
-#announcement were listed. Stock splits with such an event were removed from the data set
+#After looking up all confounding events (provided by Capital IQ) for each equity and mapping them around the 10-day event window for each stock split,
+#stock splits with a match in their event window were removed from the data set.
 
 indx <- apply(merged_df_11[,32:42], 1, function(x) all(is.na(x)))
 confounding_10days <- merged_df_11[indx,]
@@ -217,12 +220,16 @@ confounding_10days_small <- confounding_10days[,c(1:19)]
 
 confounding_10days_small$market_cap <- 
   as.numeric(as.character(confounding_10days_small$shares_oustanding_in_ks))*1000*as.numeric(as.character(confounding_10days_small$share_price))
+#Calculation of market cap [
 
 non_conf_df <- confounding_10days_small[as.numeric(as.character(confounding_10days_small$factor_to_adjust_price_FACPR))>0.25,]
 non_conf_large <- non_conf_df[non_conf_df$market_cap>0,]
+#Only onsidering split ratios >1.25
+#Only considering positive market cap (sanity check / safety)
+#MCAP = TOGGLE FOR SMALLER DATA SAMPLE WITH LARGE MCAP / SAMPLE2]
 final_df<-non_conf_large
 
-#Due to insufficient return data (only 52 returns)in the estimation window "FARMERS & MERCHANTS BANCORP INC" is removed
+#Due to insufficient return data (only 52 returns in estimation window - see below)"FARMERS & MERCHANTS BANCORP INC" is removed
 final_df <- final_df[!final_df$ticker=="FMAO",]
 
 #Conversion from factors to numeric variables
@@ -231,21 +238,22 @@ final_df$trading_volume <- as.numeric(as.character(final_df$trading_volume))
 final_df$permno <- as.numeric(as.character(final_df$permno))
 
 
-# Return / Market data for split titles ---------------------------------------------------------------------------
+# Return / market data for the estimation of abnormal returns of the split titles ------------------------
 
 #Market return during 2000-2020 (NYSE,AMEX,NASDAQ - value weighted)
 #@https://wrds-web.wharton.upenn.edu/wrds//ds/crsp/indexes_a/mktindex/cap_d.cfm
-#market index -> justify it
+#To accurately reflect the broad market which was initially considered, a value weighted market index of 
+#all three main exchanges was considered as market index
 
 market_return_df <- read.csv("market_returns.csv")
 colnames(market_return_df)[1]<-"date"
 market_return_df$date <- as.Date(format(market_return_df$date , format = '%Y%m%d'), format = '%Y%m%d')
 
-# Daily stock returns: direct SQL querry from WRDS 
-# -> requires an account and storage of account name / PW on local file, see below:
-# @https://wrds-www.wharton.upenn.edu/pages/support/programming-wrds/programming-r/r-from-your-computer/
-# for the querry structure, see below
-# @https://wrds-www.wharton.upenn.edu/pages/support/programming-wrds/programming-r/querying-wrds-data-r/#introduction
+#Daily stock returns / stock prices and traiding days: direct SQL querry from CRSP through WRDS 
+#-> requires an account and storage of account name / PW on local file, see below:
+#@https://wrds-www.wharton.upenn.edu/pages/support/programming-wrds/programming-r/r-from-your-computer/
+#for the querry structure, see below
+#@https://wrds-www.wharton.upenn.edu/pages/support/programming-wrds/programming-r/querying-wrds-data-r/#introduction
 
 
 wrds <- dbConnect(Postgres(),
@@ -255,9 +263,11 @@ wrds <- dbConnect(Postgres(),
                   sslmode='require',
                   user='dajulian')
 
-# Finding the trading days for the entire cross-section of 
-# stocks listed on the New York Stock Exchange (NYSE), t
-# he American Stock Exchange (AMEX), and the Nasdaq National Market System (NMS)
+#Sanity check to ensure that all stock split announcements happened on trading days
+
+#Finding the trading days for the entire cross-section of 
+#stocks listed on the New York Stock Exchange (NYSE), the
+#American Stock Exchange (AMEX), and the Nasdaq National Market System (NMS)
 #with the  NYSE Trade and Quote (TAQ) database
 #@https://onnokleen.de/post/taq_via_wrds/ :
 
@@ -277,15 +287,25 @@ dates_trades <-
   
 final_df$declaration_date <- as.Date(format(final_df$declaration_date, format = '%Y%m%d'), format = '%Y%m%d')
 
+# -------------------------------------------------------------------------
+# Eventstudy --------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+
+# Date preparation ---------------------------------------------------------------------------------------
+
 #Check if all declaration dates are trading dates on the exchanges
 all(final_df$declaration_date %in% dates_trades)
 
-#Event window
+#Event window definition (query only works with calendar days)
 final_df$start_date <- final_df$declaration_date-10
 final_df$end_date <-final_df$declaration_date+10
 
-#Start of estimation period
+#Start of estimation period (as far ahead as possible to ensure that enough trading days can be found to estimate beta)
 final_df$est_start_date <-final_df$declaration_date-220
+
+
+# Return gathering for event window ----------------------------------------------------------------------
 
 get_stock_return_until_announcement <- function(data.frame) {
   data_1<- tryCatch(
@@ -374,6 +394,9 @@ eventwindow_df <- rbind(return_df_until_announcement,return_df_after_announcemen
 colnames(eventwindow_df)<-final_df$ticker
 rownames(eventwindow_df)<- c("t=-5","t=-4","t=-3","t=-2","t=-1","t=0","t=+1","t=+2","t=+3","t=+4","t=+5")
 
+
+# Beta estimation and abnormal return caluclation in event window-----------------------------------------
+
 get_stock_beta_from_estimation <- function(data.frame) {
   data_1<- tryCatch(
     {
@@ -424,9 +447,11 @@ alphas_betas_df <- as.data.frame(alphas_betas_df)
 
 rownames(alphas_betas_df)<-c("alpha","beta")
 colnames(alphas_betas_df)<-final_df$ticker
-
 abnormal_ret_estimw_df <- as.data.frame(abnormal_ret_estimw_df)
 colnames(abnormal_ret_estimw_df)<-final_df$ticker
+
+
+# Variance estimation from estimation window -------------------------------------------------------------
 
 ar_variances_EW<-apply(abnormal_ret_estimw_df,2,var)
 
@@ -454,18 +479,21 @@ for (i in 1:nrow(final_df)){
 }
 
 
-# Tests / Regressions and Analysis --------------------------------------------------------------------------------
+# Tests / regressions and analysis --------------------------------------------------------------------------------
 
+#Definition of different event windows [abnormal return data frames (eventwindow X stocks) = car_df]
 car_df_orig <- eventwindow_df - market_model_df
 car_df_3 <- car_df_orig[3:9,]
 car_df_1 <- car_df_orig[5:7,]
 list_1<-list(car_df_orig,car_df_3,car_df_1)
 
+#Definition of different event windows
 days1<- -5:5
 days2<- -3:3
 days3<- -1:1
 list_2<-list(days1,days2,days3)
 
+#Definition of different cumulative abnormal returns
 CAR_11 <- apply(car_df_orig,2,sum)
 CAR_7 <-apply(car_df_3,2,sum)
 CAR_3 <-apply(car_df_1,2,sum)
@@ -473,24 +501,26 @@ list_3<-list(CAR_11,CAR_7,CAR_3)
 
 EW_length<-c(11,7,3)
 
-
+#T-tests to test for different eventwindows (CAR / CAAR)
 for(a in 1:3){
   
   car_df<-as.data.frame(list_1[a])
   sd_ew <- SD_s[a]
   days_EW <- list_2[a]
   
+  #Variance estimation based on cumulative abnormal returns
   AAR <- apply(car_df, 1, mean)
   AR_AAR_df<- (car_df - AAR)^2
   daily_variance<- apply(AR_AAR_df,1,sum)*(1/(nrow(final_df)-1))
   daily_sd<-daily_variance^0.5
+  
   
   daily_t_Values<-AAR/(daily_sd/sqrt(nrow(final_df)))
   p_values_daily <- rep(NA,nrow(car_df))
   for(i in 1:nrow(car_df)){
     p_values_daily[i] <-2*pt(-abs(daily_t_Values[i]),df=nrow(final_df)-1)
   }
-  
+  #T.test on average cumulative abnormal return
   CAR_table_out<-as.data.frame(matrix(NA,ncol = 5, nrow = nrow(car_df)))
   colnames(CAR_table_out) <- c("Days","Average Abnormal Returns","Percentage of which positive","T-statistic","p-value")
   
@@ -501,12 +531,11 @@ for(a in 1:3){
   CAR_table_out[,5] <- p_values_daily
   
   CAR <- apply(car_df,2,sum)
-  ACAR <- mean(CAR)
+  ACAR <- mean(CAR) #CAAR
   var_CAR<-(1/nrow(final_df)^2)*sum((CAR-ACAR)^2)
   sd_CAR<-var_CAR^0.5
   cum_t_val<-ACAR/sd_CAR
   cum_p_value<-2*pt(-abs(cum_t_val),df=nrow(final_df)-1)
-  
   cum_t_val_with_acar_var<-ACAR/sd_ew
   cum_p_val_with_acar_var<-2*pt(-abs(cum_t_val_with_acar_var),df=nrow(final_df)-1)
   
@@ -524,7 +553,7 @@ for(a in 1:3){
   # write.csv(cum_stats,name22)
 }
 
-# Regression variables
+#Regression variables:
 regression_df <- final_df
 #1.1 Split factor
 regression_df$factor_to_adjust_shares_FACSHR<-as.numeric(regression_df$factor_to_adjust_shares_FACSHR)+1
@@ -575,8 +604,7 @@ regression_df$equity_value <- regression_df$pre_split_price*as.numeric(regressio
 regression_df$regression_NSYE <- ifelse(regression_df$exchange_code==1,1,0)
 regression_df$regression_AMEX <- ifelse(regression_df$exchange_code==2,1,0)
 
-
-
+#Regressoin on different event windows (CAAR)
 for(b in 1:3){
   c<-EW_length[b]
   regressand<-unlist(list_3[b])
